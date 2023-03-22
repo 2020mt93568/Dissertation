62d5be82f97a33a7122a4b1534d1925b732767a0
2020mt93568 <2020mt93568@wilp.bits-pilani.ac.in>
2023-03-23 01:40:25 +0530

    added Proxy(Observable) support to state, simplifies the initialization and use of the library

--- Framework/framework.js
+++ Framework/framework.js
@@ -1,44 +1,59 @@
-function interpolate(state) {
-    for (var interpolateElement of document.getElementsByTagName("interpolate")) {
-        var content = document.createTextNode(state[interpolateElement.getAttribute("value")] || "");
-        if (interpolateElement.hasChildNodes()) {
-            interpolateElement.replaceChild(content, interpolateElement.childNodes[0]);
-        }
-        interpolateElement.appendChild(content);
-    }
-}
-
-function bindIf(state) {
-    for (var bindIfElement of document.getElementsByTagName("bindIf")) {
-
-        var bindIfElementValue = bindIfElement.getAttribute("value");
-        var isBindIfElementShown;
-
-        if (bindIfElementValue.charAt(0) == "!") {
-            isBindIfElementShown = !state[bindIfElementValue.substring(1)];
-        } else {
-            isBindIfElementShown = state[bindIfElementValue];
-        }
-
-        if (isBindIfElementShown) {
-            bindIfElement.style.display = "initial";
-        } else {
-            bindIfElement.style.display = "none";
-        }
-    }
-}
-
-function dataBind(state) {
-    for(var dataBindElement of document.querySelectorAll("[data-bind]")) {
-        dataBindElement.addEventListener("input", () => {
-            state[dataBindElement.getAttribute("data-bind")] = dataBindElement.value;
-            interpolate(state);
-        });
-    }
-}
-
 var framework = {
-    interpolate: interpolate,
-    bindIf: bindIf,
-    dataBind: dataBind
+    // Call the initialize function to get an observable Proxy to the state for the page.
+    // Any change to the state variable also rerenders the changes to HTML.
+    initialize: function () {
+        var _state = {};
+        var state = new Proxy(_state, {
+            set: function(target, prop, value) {
+              Reflect.set(target, prop, value);
+              this.update(_state);
+            }.bind(this)
+          });
+        this.update(_state);
+        return state;
+    },
+    update: function(state) {
+        this.interpolate(state);
+        this.bindIf(state);
+        this.dataBind(state);
+    },
+    // Does String Interpolation b/w JS and HTML Template.
+    interpolate: function (state) {
+        for (var interpolateElement of document.getElementsByTagName("interpolate")) {
+            var content = document.createTextNode(state[interpolateElement.getAttribute("value")] || "");
+            if (interpolateElement.hasChildNodes()) {
+                interpolateElement.replaceChild(content, interpolateElement.childNodes[0]);
+            }
+            interpolateElement.appendChild(content);
+        }
+    },
+    // Does conditional render of Nodes in HTML Template dynamically based on the value is JS.
+    bindIf: function (state) {
+        for (var bindIfElement of document.getElementsByTagName("bindIf")) {
+
+            var bindIfElementValue = bindIfElement.getAttribute("value");
+            var isBindIfElementShown;
+
+            if (bindIfElementValue.charAt(0) == "!") {
+                isBindIfElementShown = !state[bindIfElementValue.substring(1)];
+            } else {
+                isBindIfElementShown = state[bindIfElementValue];
+            }
+
+            if (isBindIfElementShown) {
+                bindIfElement.style.display = "initial";
+            } else {
+                bindIfElement.style.display = "none";
+            }
+        }
+    },
+    //  Does Data Binding b/w HTML Template and JS.
+    dataBind: function (state) {
+        for (var dataBindElement of document.querySelectorAll("[data-bind]")) {
+            dataBindElement.addEventListener("input", function() {
+                state[dataBindElement.getAttribute("data-bind")] = dataBindElement.value;
+                this.interpolate(state);
+            }.bind(this));
+        }
+    },
 };
